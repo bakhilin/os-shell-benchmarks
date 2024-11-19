eab36a97938f1c0a22118e7bc5bd12ad3c5e6bc4
Bakhilin Nikita <bakhilinnikita04@mail.ru>
2024-11-19 19:47:51 +0300

    modify ci

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@ git add .
 git commit -m "modify ci"
 git push
 # g++ -o test_sort src/sort.c  src/test_sort.cpp -lgtest -lgtest_main -pthread
-
+# g++ -o test_search_str src/test_search_str.cpp -lpthread -lgtest -lgtest_main -pthread
